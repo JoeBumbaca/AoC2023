1ee7de2e3685f25e8241a7492c41ae5580e3c75d
Joe Bumbaca <joebumbaca@Joes-MacBook-Pro.local>
2023-12-21 16:09:58 -0800

    start day1

---
+++ D01/solution.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const text = fs.readFileSync('./input.txt', 'utf-8');
+let input = text.split('\n');
+
+// console.log(input);
+
+
+const calibrationSum = (codes) => {
+    let sum = 0;
+    let nums = [0,1,2,3,4,5,6,7,8,9,0];
+    
+
+
+}
